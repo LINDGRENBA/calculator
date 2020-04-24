ae1d8b666d0b5794a59b95556c01105f5aa13310
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-04-23 20:36:37 -0700

    Add html form, update UI logic in scripts with .ready function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
+// BUSINESS LOGIC
+
 var add = function(number1, number2) {
   return number1 + number2;
-  };
-  
+};
+
+// USER INTERFACE
+
+$(document).ready(function() {
   var number1 = parseInt(prompt("Enter a number:"));
   var number2 = parseInt(prompt("Enter another number:"));
   
   alert(add(number1, number2));
+});
+
+
