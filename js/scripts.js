6b9bab9a6bb837db437e2ead4bac846e525c852e
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-04-24 14:36:58 -0700

    Update with subtract, multiply and divide capability.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,57 @@
 // BUSINESS LOGIC
 
-var add = function(number1, number2) {
+let add = function(number1, number2) {
   return number1 + number2;
 };
+
+let subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+let multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+let divide = function(number1, number2) {
+  return number1 / number2;
+};
+
 
 // USER INTERFACE
 
 $(document).ready(function() {
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
-  
-  alert(add(number1, number2));
+  // ADD
+  $("#add").submit(function(event) {
+    event.preventDefault();
+    let number1 = parseInt($("#add1").val());
+    let number2 = parseInt($("#add2").val());
+    let result = add(number1, number2);
+    $("#output").text(result);
+  });
+  // SUBTRACT
+  $("#subtract").submit(function(event) {
+    event.preventDefault();
+    let number1 = parseInt($("#subtract1").val());
+    let number2 = parseInt($("#subtract2").val());
+    let result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+  // MULTIPLY
+  $("#multiply").submit(function(event) {
+    event.preventDefault();
+    let number1 = parseInt($("#multiply1").val());
+    let number2 = parseInt($("#multiply2").val());
+    let result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+  // DIVIDE
+  $("#divide").submit(function(event) {
+    event.preventDefault();
+    let number1 = parseInt($("#divide1").val());
+    let number2 = parseInt($("#divide2").val());
+    let result = divide(number1, number2);
+    $("#output").text(result);
+  });
 });
 
 
