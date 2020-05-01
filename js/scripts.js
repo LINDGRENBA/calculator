bf4252a3b3558ca7bfb2ceb2ee37b78f9dcb4736
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-05-01 14:34:07 -0700

    Update js to refactor code, make js and jQuery more efficient.

--- js/scripts.js
+++ js/scripts.js
@@ -20,38 +20,58 @@ let divide = function(number1, number2) {
 // USER INTERFACE
 
 $(document).ready(function() {
-  // ADD
-  $("#add").submit(function(event) {
+  $("form#calculator").submit(function(event) {
     event.preventDefault();
-    let number1 = parseInt($("#add1").val());
-    let number2 = parseInt($("#add2").val());
-    let result = add(number1, number2);
-    $("#output").text(result);
-  });
-  // SUBTRACT
-  $("#subtract").submit(function(event) {
-    event.preventDefault();
-    let number1 = parseInt($("#subtract1").val());
-    let number2 = parseInt($("#subtract2").val());
-    let result = subtract(number1, number2);
-    $("#output").text(result);
-  });
-  // MULTIPLY
-  $("#multiply").submit(function(event) {
-    event.preventDefault();
-    let number1 = parseInt($("#multiply1").val());
-    let number2 = parseInt($("#multiply2").val());
-    let result = multiply(number1, number2);
-    $("#output").text(result);
-  });
-  // DIVIDE
-  $("#divide").submit(function(event) {
-    event.preventDefault();
-    let number1 = parseInt($("#divide1").val());
-    let number2 = parseInt($("#divide2").val());
-    let result = divide(number1, number2);
-    $("#output").text(result);
+    let number1 = parseFloat($("#number1").val());
+    let number2 = parseFloat($("#number2").val());
+    let operator = $("input:radio[name=operator]:checked").val();
+    let result;
+      if(operator === "add") {
+        result = add(number1, number2)
+      } else if(operator === "subtract") {
+        result = subtract(number1, number2)
+      } else if(operator === "multiply") {
+        result = multiply(number1, number2)
+      } else if(operator === "divide") {
+        result = divide(number1, number2)
+      }
+    $("#output").text(result.toFixed(2));
   });
 });
+
+// $(document).ready(function() {
+//   // ADD
+//   $("#add").submit(function(event) {
+//     event.preventDefault();
+//     let number1 = parseInt($("#add1").val());
+//     let number2 = parseInt($("#add2").val());
+//     let result = add(number1, number2);
+//     $("#output").text(result);
+//   });
+//   // SUBTRACT
+//   $("#subtract").submit(function(event) {
+//     event.preventDefault();
+//     let number1 = parseInt($("#subtract1").val());
+//     let number2 = parseInt($("#subtract2").val());
+//     let result = subtract(number1, number2);
+//     $("#output").text(result);
+//   });
+//   // MULTIPLY
+//   $("#multiply").submit(function(event) {
+//     event.preventDefault();
+//     let number1 = parseInt($("#multiply1").val());
+//     let number2 = parseInt($("#multiply2").val());
+//     let result = multiply(number1, number2);
+//     $("#output").text(result);
+//   });
+//   // DIVIDE
+//   $("#divide").submit(function(event) {
+//     event.preventDefault();
+//     let number1 = parseInt($("#divide1").val());
+//     let number2 = parseInt($("#divide2").val());
+//     let result = divide(number1, number2);
+//     $("#output").text(result);
+//   });
+// });
 
 
