d41e9e5d88f06c9830a4ee337e0d4c0d8797b4bc
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-05-01 14:36:13 -0700

    Delete commented out lines of js and jQuery from previous version.

--- js/scripts.js
+++ js/scripts.js
@@ -39,39 +39,6 @@ $(document).ready(function() {
   });
 });
 
-// $(document).ready(function() {
-//   // ADD
-//   $("#add").submit(function(event) {
-//     event.preventDefault();
-//     let number1 = parseInt($("#add1").val());
-//     let number2 = parseInt($("#add2").val());
-//     let result = add(number1, number2);
-//     $("#output").text(result);
-//   });
-//   // SUBTRACT
-//   $("#subtract").submit(function(event) {
-//     event.preventDefault();
-//     let number1 = parseInt($("#subtract1").val());
-//     let number2 = parseInt($("#subtract2").val());
-//     let result = subtract(number1, number2);
-//     $("#output").text(result);
-//   });
-//   // MULTIPLY
-//   $("#multiply").submit(function(event) {
-//     event.preventDefault();
-//     let number1 = parseInt($("#multiply1").val());
-//     let number2 = parseInt($("#multiply2").val());
-//     let result = multiply(number1, number2);
-//     $("#output").text(result);
-//   });
-//   // DIVIDE
-//   $("#divide").submit(function(event) {
-//     event.preventDefault();
-//     let number1 = parseInt($("#divide1").val());
-//     let number2 = parseInt($("#divide2").val());
-//     let result = divide(number1, number2);
-//     $("#output").text(result);
-//   });
-// });
+
 
 
